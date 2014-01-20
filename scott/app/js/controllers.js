dc3f30dcecd5d2c57d714e1718d84f2a5206407e
Sebastiaan van Steenis <sebastiaan.van.steenis@kvk.nl>
2014-01-20 17:20:10 +0100

    [ZMMPF-46] Mogelijkheid tot aangeven DB2/MQ/IL in Portaal

--- scott/app/js/controllers.js
+++ scott/app/js/controllers.js
@@ -27,7 +27,7 @@ angular.module('myApp.controllers', []).
   }])
   .controller('Aanvragen', [ '$scope', '$http',
     function($scope, $http) {
-      $scope.aanvraag = { 'action':'create' };
+      $scope.aanvraag = { 'action':'create', 'includedb2':'false', 'includemq':'false', 'includeil':'false' };
       $scope.submitaanvraag = function() {
         $http({
           method : 'POST',
