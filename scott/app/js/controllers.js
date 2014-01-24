8cd491477492f8c2570b93dfc37b83414c57185e
Sebastiaan van Steenis <sebastiaan.van.steenis@kvk.nl>
2014-01-24 11:31:36 +0100

    [ZMMPF-46] Another nested hash

--- scott/app/js/controllers.js
+++ scott/app/js/controllers.js
@@ -27,7 +27,7 @@ angular.module('myApp.controllers', []).
   }])
   .controller('Aanvragen', [ '$scope', '$http',
     function($scope, $http) {
-      $scope.aanvraag = { 'action':'create', 'db2' : { 'include':'false'}, 'mq': { 'include':'false'}, 'il': { 'include':'false' } };
+      $scope.aanvraag = { 'action':'create', 'middleware' : { 'db2' : { 'include':'false'}, 'mq': { 'include':'false'}, 'il': { 'include':'false' } } };
       $scope.submitaanvraag = function() {
         $http({
           method : 'POST',
